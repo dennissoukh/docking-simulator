5f2d30a5562d045672165ade652b863b0c4507a0
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-12-31 00:46:39 +0000

    refactor

--- Assets/KeybindsButtonHandler.cs
+++ Assets/KeybindsButtonHandler.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 
 public class KeybindsButtonHandler : MonoBehaviour
 {
-    private GameObject KeybindsMenu;
-    private Button Button;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Button = gameObject.GetComponent<Button>();
-        KeybindsMenu = GameObject.Find("KeybindsMenu");
-        Button.onClick.AddListener(HandleKeybindsMenuClick);
-    }
+  private GameObject KeybindsMenu;
+  private Button Button;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Button = gameObject.GetComponent<Button>();
+    KeybindsMenu = GameObject.Find("KeybindsMenu");
+    Button.onClick.AddListener(HandleKeybindsMenuClick);
+  }
 
-    void HandleKeybindsMenuClick()
-    {
-        if (KeybindsMenu != null) KeybindsMenu.GetComponentInChildren<Canvas>().enabled = true;
-        
-        var menu = GameObject.Find("SceneLoader").GetComponentInChildren<Canvas>();
-        menu.enabled = false;
-    }
+  void HandleKeybindsMenuClick()
+  {
+    if (KeybindsMenu != null) KeybindsMenu.GetComponentInChildren<Canvas>().enabled = true;
+
+    var menu = GameObject.Find("SceneLoader").GetComponentInChildren<Canvas>();
+    menu.enabled = false;
+  }
 }
--- Assets/SpacecraftScript.cs
+++ Assets/SpacecraftScript.cs
@@ -4,127 +4,127 @@ using UnityEngine;
 
 public class SpacecraftScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private GameObject NoseCam;
-    private GameObject DockingPortCam;
-    private GameObject ThirdPersonCam;
-    Rigidbody SpacecraftRB;
+  // Start is called before the first frame update
+  private GameObject NoseCam;
+  private GameObject DockingPortCam;
+  private GameObject ThirdPersonCam;
+  Rigidbody SpacecraftRB;
 
-    private GameObject SpaceStation;
+  private GameObject SpaceStation;
 
-    private float ManeuverSpeedMultiplier, ForceMultiplier;
+  private float ManeuverSpeedMultiplier, ForceMultiplier;
 
-    public float SpacecraftSpeed, SpacecraftPitch, SpacecraftYaw, Range;
+  public float SpacecraftSpeed, SpacecraftPitch, SpacecraftYaw, Range;
 
 
-    void Start()
-    {
-        NoseCam = GameObject.Find("NoseCamera");
-        DockingPortCam = GameObject.Find("DockingPortCamera");
-        ThirdPersonCam = GameObject.Find("ThirdPersonCamera");
-        SpaceStation = GameObject.Find("ISS_stationary");
-        SpacecraftRB = this.GetComponent<Rigidbody>();
-        SpacecraftYaw = SpacecraftPitch = SpacecraftSpeed = 0;
-        ManeuverSpeedMultiplier = 15000f;
-        ForceMultiplier = 0.1f;
-        SetCamNose();
-    }
+  void Start()
+  {
+    NoseCam = GameObject.Find("NoseCamera");
+    DockingPortCam = GameObject.Find("DockingPortCamera");
+    ThirdPersonCam = GameObject.Find("ThirdPersonCamera");
+    SpaceStation = GameObject.Find("ISS_stationary");
+    SpacecraftRB = this.GetComponent<Rigidbody>();
+    SpacecraftYaw = SpacecraftPitch = SpacecraftSpeed = 0;
+    ManeuverSpeedMultiplier = 15000f;
+    ForceMultiplier = 0.1f;
+    SetCamNose();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckControls();
-        UpdateUIElements();
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    CheckControls();
+    UpdateUIElements();
+  }
 
-    void FixedUpdate()
-    {
+  void FixedUpdate()
+  {
 
-    }
+  }
 
-    void UpdateUIElements()
-    {
-        SpacecraftSpeed = SpacecraftRB.velocity.magnitude;
-        SpacecraftPitch = this.transform.rotation.x;
-        SpacecraftYaw = this.transform.rotation.z;
-        Range = (this.transform.position - SpaceStation.transform.position).magnitude;
-    }
+  void UpdateUIElements()
+  {
+    SpacecraftSpeed = SpacecraftRB.velocity.magnitude;
+    SpacecraftPitch = this.transform.rotation.x;
+    SpacecraftYaw = this.transform.rotation.z;
+    Range = (this.transform.position - SpaceStation.transform.position).magnitude;
+  }
 
-    void CheckControls()
-    {
-        if (Input.GetKey(KeyCode.W)) ForwardThrust();
-        if (Input.GetKey(KeyCode.S)) BackwardThrust();
-        if (Input.GetKey(KeyCode.Q)) UpwardsThrust();
-        if (Input.GetKey(KeyCode.E)) DownwardsThrust();
-        if (Input.GetKey(KeyCode.UpArrow)) IncreasePitch();
-        if (Input.GetKey(KeyCode.DownArrow)) DecreasePitch();
-        if (Input.GetKey(KeyCode.LeftArrow)) RollLeft();
-        if (Input.GetKey(KeyCode.RightArrow)) RollRight();
-        if (Input.GetKey(KeyCode.Alpha1)) SetCamNose();
-        if (Input.GetKey(KeyCode.Alpha2)) SetCamDockingPort();
-        if (Input.GetKey(KeyCode.Alpha3)) SetCamThirdPerson();
-    }
+  void CheckControls()
+  {
+    if (Input.GetKey(KeyCode.W)) ForwardThrust();
+    if (Input.GetKey(KeyCode.S)) BackwardThrust();
+    if (Input.GetKey(KeyCode.Q)) UpwardsThrust();
+    if (Input.GetKey(KeyCode.E)) DownwardsThrust();
+    if (Input.GetKey(KeyCode.UpArrow)) IncreasePitch();
+    if (Input.GetKey(KeyCode.DownArrow)) DecreasePitch();
+    if (Input.GetKey(KeyCode.LeftArrow)) RollLeft();
+    if (Input.GetKey(KeyCode.RightArrow)) RollRight();
+    if (Input.GetKey(KeyCode.Alpha1)) SetCamNose();
+    if (Input.GetKey(KeyCode.Alpha2)) SetCamDockingPort();
+    if (Input.GetKey(KeyCode.Alpha3)) SetCamThirdPerson();
+  }
 
-    //Maneuvering spacecraft
-    void ForwardThrust()
-    {
-        SpacecraftRB.velocity = this.transform.forward * ForceMultiplier + SpacecraftRB.velocity;
-    }
+  //Maneuvering spacecraft
+  void ForwardThrust()
+  {
+    SpacecraftRB.velocity = this.transform.forward * ForceMultiplier + SpacecraftRB.velocity;
+  }
 
-    void BackwardThrust()
-    {
-        SpacecraftRB.velocity = -this.transform.forward * ForceMultiplier + SpacecraftRB.velocity;
-    }
+  void BackwardThrust()
+  {
+    SpacecraftRB.velocity = -this.transform.forward * ForceMultiplier + SpacecraftRB.velocity;
+  }
 
-    void UpwardsThrust()
-    {
-       SpacecraftRB.velocity = this.transform.up * ForceMultiplier + SpacecraftRB.velocity;
-    }
+  void UpwardsThrust()
+  {
+    SpacecraftRB.velocity = this.transform.up * ForceMultiplier + SpacecraftRB.velocity;
+  }
 
-    void DownwardsThrust()
-    {
-       SpacecraftRB.velocity = -this.transform.up * ForceMultiplier + SpacecraftRB.velocity;
-    }
+  void DownwardsThrust()
+  {
+    SpacecraftRB.velocity = -this.transform.up * ForceMultiplier + SpacecraftRB.velocity;
+  }
 
-    void RollLeft()
-    {
-        SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * this.transform.forward);
-    }
+  void RollLeft()
+  {
+    SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * this.transform.forward);
+  }
 
-    void RollRight()
-    {
-        SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * -this.transform.forward);
-    }
+  void RollRight()
+  {
+    SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * -this.transform.forward);
+  }
 
-    void IncreasePitch()
-    {
-        SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * this.transform.right);
-    }
+  void IncreasePitch()
+  {
+    SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * this.transform.right);
+  }
 
-    void DecreasePitch()
-    {
-        SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * -this.transform.right);
-    }
+  void DecreasePitch()
+  {
+    SpacecraftRB.AddTorque(ManeuverSpeedMultiplier * ForceMultiplier * -this.transform.right);
+  }
 
-    //Cameras
-    void SetCamNose()
-    {
-        DockingPortCam.SetActive(false);
-        ThirdPersonCam.SetActive(false);
-        NoseCam.SetActive(true);
-    }
+  //Cameras
+  void SetCamNose()
+  {
+    DockingPortCam.SetActive(false);
+    ThirdPersonCam.SetActive(false);
+    NoseCam.SetActive(true);
+  }
 
-    void SetCamDockingPort()
-    {
-        NoseCam.SetActive(false);
-        ThirdPersonCam.SetActive(false);
-        DockingPortCam.SetActive(true);
-    }
+  void SetCamDockingPort()
+  {
+    NoseCam.SetActive(false);
+    ThirdPersonCam.SetActive(false);
+    DockingPortCam.SetActive(true);
+  }
 
-    void SetCamThirdPerson()
-    {
-        NoseCam.SetActive(false);
-        DockingPortCam.SetActive(false);
-        ThirdPersonCam.SetActive(true);
-    }
+  void SetCamThirdPerson()
+  {
+    NoseCam.SetActive(false);
+    DockingPortCam.SetActive(false);
+    ThirdPersonCam.SetActive(true);
+  }
 }
