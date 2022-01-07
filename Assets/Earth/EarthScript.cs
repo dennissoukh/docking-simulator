7c659ddb471586f40c3050aedbf98390d3bef4b1
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2022-01-07 06:26:43 +0000

    add earth

---
+++ Assets/Earth/EarthScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthScript : MonoBehaviour
+{
+  void Start()
+  {
+    GetComponent<Rigidbody>().AddTorque(-transform.up * 500000000f);
+  }
+
+  void OnCollisionEnter(Collision col)
+  {
+    if (GameManagerScript.GameState == 0 && col.gameObject.name == "Spacecraft")
+    {
+      GameManagerScript.DockUnsuccessful("Got too close to earth");
+    }
+  }
+}
