79159a730e7da176eb8508519911655013a791d6
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2022-01-07 23:06:04 +0000

    docking port trigger & settings menu

--- Assets/Scripts/SettingsMenuHandler.cs
+++ Assets/Scripts/SettingsMenuHandler.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenuHandler : MonoBehaviour
 {
   private Canvas menu;
   private CanvasGroup group;
+  public AudioMixer audioMixer;
+  public Slider volumeSlider;
+  float currentVolume;
 
+  public AudioMixer mixer;
+
+  public void SetVolume(float sliderValue) {
+    mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+  }
+
+  public void SaveSettings()
+  {
+    PlayerPrefs.SetFloat("VolumePreference", currentVolume);
+  }
   // Start is called before the first frame update
   void Awake()
   {
